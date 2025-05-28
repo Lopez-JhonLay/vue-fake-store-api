77a2dde1a7a0fac0662c03abfaec9e1cc0eccc6d
Jhon Lay Lopez <Jhon.Lopez@tbu.net>
2025-05-28 14:58:28 +0800

    feat: add axios for API calls and implement product listing with Element Plus components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,87 @@
 <template>
-  <div></div>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <div><h3>Pekeng Tindahan</h3></div>
+        <div></div>
+      </el-header>
+      <el-divider />
+      <el-main>
+        <el-row :gutter="20">
+          <el-col
+            v-for="product in products"
+            :key="product.id"
+            :xs="24"
+            :sm="24"
+            :md="12"
+            :lg="8"
+            :xl="4"
+          >
+            <ProductCard
+              :id="product.id"
+              :title="product.title"
+              :image="product.image"
+              :description="product.description"
+              :price="product.price"
+            />
+          </el-col>
+        </el-row>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
 
-<style scoped></style>
+import axios from 'axios';
+
+import type { Product } from '@/models/product';
+
+import ProductCard from './components/ProductCard.vue';
+
+const products = ref<Product[]>([]);
+const isLoading = ref<boolean>(false);
+
+const loadProducts = async () => {
+  try {
+    isLoading.value = true;
+    const response = await axios.get<Product[]>('https://fakestoreapi.com/products');
+    products.value = response.data;
+    console.log('Products response data:', response.data);
+    console.log('Products:', products.value);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(() => {
+  loadProducts();
+});
+</script>
+
+<style scoped>
+:deep(.el-container) {
+  width: 100%;
+  max-width: 1800px;
+  height: 100vh;
+  margin: 0 auto;
+}
+
+:deep(.el-header) {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+:deep(.el-divider) {
+  margin: 0;
+}
+
+.el-col {
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-card class="product-card" shadow="hover">
+    <el-image :src="props.image" fit="contain" alt="{{ props.name }}" draggable="false" hover />
+    <template #footer>
+      <el-text class="truncate-text">{{ props.title }}</el-text>
+      <el-text type="success">$ {{ props.price.toLocaleString() }}</el-text>
+      <div>
+        <el-button class="add-to-cart-btn">Add to Cart</el-button>
+      </div>
+    </template>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category?: string;
+  image?: string;
+  rating?: {
+    rate: number;
+    count: number;
+  };
+}>();
+</script>
+
+<style scoped>
+:deep(.el-image__inner) {
+  max-width: 100%;
+  aspect-ratio: 3/2;
+}
+
+.truncate-text {
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  line-clamp: 1;
+  box-orient: vertical;
+}
+
+.add-to-cart-btn {
+  width: 100%;
+  background-color: #409eff;
+  color: white;
+  border-radius: 4px;
+  text-align: center;
+  margin-top: 0.5rem;
+}
+</style>
