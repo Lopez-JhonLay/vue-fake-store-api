0fe999bd396fa0e97e2b096708745cede4ab91f1
Jhon Lay Lopez <Jhon.Lopez@tbu.net>
2025-05-29 16:33:26 +0800

    feat: implement CartDrawer component and integrate with HeaderComponent for cart functionality

---
+++ src/components/CartDrawer.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-drawer v-model="isCartDrawerOpen" title="I am the title" direction="rtl">
+    <span>Hi, there!</span>
+  </el-drawer>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const props = defineProps<{
+  modelValue?: boolean;
+}>();
+
+const emit = defineEmits<{ (e: 'update:modelValue', value: boolean): void }>();
+
+const isCartDrawerOpen = ref(props.modelValue);
+
+watch(isCartDrawerOpen, (newValue) => {
+  emit('update:modelValue', newValue);
+});
+
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    isCartDrawerOpen.value = newValue ?? false;
+  },
+  { immediate: true },
+);
+</script>
+
+<style scoped></style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,17 +2,29 @@
   <div><h3>Pekeng Tindahan</h3></div>
   <div style="display: flex; gap: 1rem">
     <el-badge :value="cartStore.totalItems" class="item" type="primary">
-      <el-button :icon="ShoppingCart">Cart</el-button>
+      <el-button :icon="ShoppingCart" @click="openCartDrawer()">Cart</el-button>
     </el-badge>
   </div>
+
+  <CartDrawer v-model="isCartDrawerOpen" />
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+import CartDrawer from './CartDrawer.vue';
+
 import { useCartStore } from '@/stores/cartStore';
 
 import { ShoppingCart } from '@element-plus/icons-vue';
 
 const cartStore = useCartStore();
+
+const isCartDrawerOpen = ref(false);
+
+const openCartDrawer = () => {
+  isCartDrawerOpen.value = true;
+};
 </script>
 
 <style scoped></style>
